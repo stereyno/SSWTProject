f37ae433031bfb7f7497b15fe6557889e17e9695
EC2 Default User <ec2-user@ip-172-31-3-111.eu-west-1.compute.internal>
2018-03-30 14:47:49 +0000

    Cart / Login Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
+# routes modified to allow direct link to home page of site
+
+
+
 Rails.application.routes.draw do
+  get '/cart', to: 'cart#index'
+
+  get '/about', to: 'static_pages#about'
+
+  get '/contact', to: 'static_pages#contact'
+
+  get '/help', to:'static_pages#help'
+  
+  get '/login', to: 'user#login'
+  
+  get '/logout', to: 'user#logout'
+
+  devise_for :users
   resources :vinyls
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'vinyls#index'
+  
 end
